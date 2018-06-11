81514016a9318548883121137549a27bc5ded5f1
Christian Barra <barrachri@gmail.com>
2018-06-11 16:16:36 +0200

    Bump up version. (#4)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = [
 
 setup(
     name="atomicpuppy_sqlcounter",
-    version="0.2",
+    version="0.21",
     install_requires=install_requires,
     py_modules=['atomicpuppy_sqlcounter'],
     url='https://github.com/madedotcom/atomicpuppy-sqlcounter',
